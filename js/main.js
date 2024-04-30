2494232c430b5c5e9623b43d272242ef3a335e55
MacNikSipeykin <niksipeykin@mail.ru>
2024-04-30 16:07:28 +0300

    create map part

--- js/main.js
+++ js/main.js
@@ -65,4 +65,37 @@ $(function () {
     }
   })
 
+
+  let map;
+
+// async function initMap() {
+//   // The location of Uluru
+//   const position = { lat: -25.344, lng: 131.031 };
+//   // Request needed libraries.
+//   //@ts-ignore
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+//   // The map, centered at Uluru
+//   map = new Map(document.getElementById("map"), {
+//     zoom: 8,
+//     center: {lat: -34.397, lng: 150.644},
+//     mapId: "DEMO_MAP_ID",
+//   });
+
+//   // The marker, positioned at Uluru
+//   const marker = new AdvancedMarkerView({
+//     map: map,
+//     position: position,
+//     title: "Uluru",
+//   });
+// }
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8,
+  });
+}
+
 })
